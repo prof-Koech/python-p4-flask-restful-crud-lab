55d6356493ff704dd78cc6292ad88de3fd602643
Kennedy <kiplangatkennedy019@gmail.com>
2023-06-24 06:15:09 +0300

    almost done with assignment

--- server/app.py
+++ server/app.py
@@ -17,36 +17,61 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
 
     def post(self):
-
         data = request.get_json()
-
         new_plant = Plant(
             name=data['name'],
             image=data['image'],
             price=data['price'],
         )
-
         db.session.add(new_plant)
         db.session.commit()
-
         return make_response(new_plant.to_dict(), 201)
 
 api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        data = request.get_json()
+        record = Plant.query.filter(Plant.id == id).first()
+
+
+        for attr in data:
+            setattr(record, attr, data[attr])
+        db.session.add(record)
+        db.session.commit()
+        response_dict = record.to_dict()
+        response = make_response(
+            response_dict,
+            200
+        )
+        return response
+
+
+    def delete(self, id):
+        record = Plant.query.filter(Plant.id == id).first()
+        db.session.delete(record)
+        db.session.commit()
+        return make_response('', 204)
+
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
+
 api.add_resource(PlantByID, '/plants/<int:id>')
         
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
